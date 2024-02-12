d0696997dc967247d7cb38ea363cdd29e37c1157
Yuri Matos Rodrigues <yurimatos.tuti@gmail.com>
2024-02-12 12:56:55 -0300

    feat: calculo da idade

--- Outside/script.js
+++ Outside/script.js
@@ -6,10 +6,11 @@ let time = 3000,
     btnBack = document.getElementById("btnBack"),
     btnNext = document.getElementById("btnNext"),
     textElement = document.querySelector('.automaticText'),    
-    textsIntro = ['developer.', 'designer.', 'musician.'],
+    textsIntro = ['Software Engineer.', 'Developer', 'Designer.', 'Musician.'],
     counter = 0;
 
-function start(){
+function start(){    
+    calcularIdade();
     setInterval(()=>{
         if(counter==textsIntro.length) counter=0;
         automaticText(textElement, textsIntro[counter]);
@@ -50,7 +51,32 @@ function automaticText(element, text){
         setTimeout(function(){
             element.innerHTML += letter;
         }, 60*i);
-    });    
+    });
+}
+
+function calcularIdade(){
+    let diaNasc = 13;
+    let mesNasc = 6;
+    let anoNasc = 1999;
+
+    let data = new Date();
+    let ano = data.getFullYear();
+    let mes = data.getMonth() + 1;
+    let dia = data.getDate();    
+
+    let idade = 0;
+    
+    idade = ano - anoNasc;
+    if(mes - mesNasc < 0 )
+    {
+        idade = idade - 1;
+    }
+    else if(mes - mesNasc == 0){
+        if(dia - diaNasc < 0){
+            idade = idade -1;
+        }
+    }
+    document.getElementById('idade').innerHTML = idade.toString();
 }
 
 window.addEventListener("load",start);
